9a3e02f087866e9fcd976074d5cf7048f04d4249
ksprasad001 <mail2k.sriprasad@gmail.com>
2018-04-08 09:31:19 -0500

    scroll to top

--- src/app/components/service/service.component.ts
+++ src/app/components/service/service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-service',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  redirect(serviceName){
+    console.log("print service", serviceName)
+    this.router.navigate(['./'+serviceName]);
   }
 
 }
