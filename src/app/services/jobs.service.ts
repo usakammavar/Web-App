2c768f4777fadb722bbda023ccd1bf7cab1a9529
ksprasad001 <mail2k.sriprasad@gmail.com>
2018-06-01 20:29:58 -0500

    css

---
+++ src/app/services/jobs.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+import {Http, Response, Headers, RequestOptions} from '@angular/http'
+import {Observable} from 'rxjs/Observable';
+import {environment} from '../../environments/environment'
+
+@Injectable()
+export class JobsService {
+
+
+  options
+  constructor(private http: Http) { 
+    let headers =new Headers({'Content-Type': 'application/json','Accept':'application/json','Access-Control-Allow-Headers': 'Content-Type'});
+    this.options= new RequestOptions({headers: headers})
+  }
+
+  getJobsList(): Observable<any> {
+    let response=this.http.get('http://kammavar.gear.host/api/Job').map(this.jobs);
+    console.log("get home response", response)
+    return response
+  }
+
+  private jobs(res: Response) {
+    let job = res.json();
+    return job || { };
+  }
+}
